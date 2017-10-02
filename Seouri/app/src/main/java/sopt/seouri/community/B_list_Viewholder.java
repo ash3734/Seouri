1159961c30c6baedd22a1cb58974b2761dca48a3
DESKTOP-7V6EF9S\yangseunghyuk <glgldf123@gmail.com>
2017-10-02 16:54:14 +0900

    틀잡기2

---
+++ Seouri/app/src/main/java/sopt/seouri/community/B_list_Viewholder.java
@@ -0,0 +1,28 @@
+package sopt.seouri.community;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import sopt.seouri.R;
+
+/**
+ * Created by yangseunghyuk on 2017-09-30.
+ */
+
+public class B_list_Viewholder extends RecyclerView.ViewHolder {
+
+    TextView B_title;
+    TextView B_writer;
+    TextView B_date;
+    TextView B_views;
+
+    public B_list_Viewholder(View itemView) {
+        super(itemView);
+
+        B_title = (TextView)itemView.findViewById(R.id.b_title);
+        B_writer = (TextView)itemView.findViewById(R.id.b_writer);
+        B_date = (TextView)itemView.findViewById(R.id.b_date);
+        B_views = (TextView)itemView.findViewById(R.id.b_views);
+    }
+}
