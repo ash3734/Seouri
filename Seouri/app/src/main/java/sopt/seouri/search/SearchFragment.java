37e26e2ab0665d93335a845d0e1764d5cfb2febd
jiwon <wlldnjs@naver.com>
2017-10-06 05:20:28 +0900

    SearchFragment - 선택한 구의 category number 전달하는 메소드 작성

--- Seouri/app/src/main/java/sopt/seouri/search/SearchFragment.java
+++ Seouri/app/src/main/java/sopt/seouri/search/SearchFragment.java
@@ -4,16 +4,19 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import sopt.seouri.R;
 import sopt.seouri.adapters.GridAdapter;
+import sopt.seouri.search.category.CategoryFragment;
+
+import static sopt.seouri.MainActivity.fragmentManager;
 
 /**
  * Created by ash on 2017-09-20.
@@ -62,7 +65,16 @@ public class SearchFragment extends Fragment {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(context, countryList[position], Toast.LENGTH_SHORT).show();
+                //Toast.makeText(context, countryList[position], Toast.LENGTH_SHORT).show();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                CategoryFragment categoryFragment = new CategoryFragment();
+//                Bundle bundle = new Bundle();
+//                bundle.putSerializable("select", countryList[position]);
+//                bundle.putSerializable("selectNum",position);
+//                categoryFragment.setArguments(bundle);
+                categoryFragment.setContext(context, position);
+                transaction.replace(R.id.container,categoryFragment);
+                transaction.commit();
             }
         });
 
