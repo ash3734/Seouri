348eba2d6e6bb0fd9d58a0225b94c3538530e09d
jiwon <wlldnjs@naver.com>
2017-10-06 05:21:50 +0900

    RecyclerView 내부에 CardView 출력 테스트 완료

---
+++ Seouri/app/src/main/java/sopt/seouri/search/category/CategoryDatas.java
@@ -0,0 +1,19 @@
+package sopt.seouri.search.category;
+
+/**
+ * Created by 김지원 on 2017-10-06.
+ */
+
+public class CategoryDatas {
+    public String id;
+    public String name;
+    public String photo;
+    public String category;
+
+    public CategoryDatas(String id, String name, String photo, String category) {
+        this.id = id;
+        this.name = name;
+        this.photo = photo;
+        this.category = category;
+    }
+}
