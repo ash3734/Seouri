b62b466d9626f15dc5734aefa13409725cffb52d
jiwon <wlldnjs@naver.com>
2017-10-06 00:45:31 +0900

    SearchFragment.java - Gridview에 리스트 출력 후 item 클릭 시 아이템 정보 출력까지 완료

--- Seouri/app/src/main/java/sopt/seouri/search/SearchFragment.java
+++ Seouri/app/src/main/java/sopt/seouri/search/SearchFragment.java
@@ -1,52 +1,71 @@
 package sopt.seouri.search;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import sopt.seouri.R;
+import sopt.seouri.adapters.GridAdapter;
 
 /**
  * Created by ash on 2017-09-20.
  */
 
 public class SearchFragment extends Fragment {
+    private Context context;
+    private GridView gridView;
+    private String[] countryList;
+
+
     /*뭘 의미하는지는 모르겠음*/
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String  mParam1;
-    private String  mParam2;
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//    private String  mParam1;
+//    private String  mParam2;
 
     public SearchFragment() {
     }
-    /*뭘 의미하는지는 모르겠음*/
-    public static SearchFragment newInstance(String param1, String param2){
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1,param1);
-        args.putString(ARG_PARAM2,param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public void setContext(Context context){
+        this.context = context;
     }
+//    /*뭘 의미하는지는 모르겠음*/
+//    public static SearchFragment newInstance(String param1, String param2){
+//        SearchFragment fragment = new SearchFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1,param1);
+//        args.putString(ARG_PARAM2,param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
+        countryList = getResources().getStringArray(R.array.seoul);
 
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.search_fragment, container, false);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.search_fragment, container, false);
+        gridView = (GridView)layout.findViewById(R.id.search_location_gridview);
+        gridView.setAdapter(new GridAdapter(context, countryList));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(context, countryList[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
-
-        return rootView;
+        return layout;
     }
 }
