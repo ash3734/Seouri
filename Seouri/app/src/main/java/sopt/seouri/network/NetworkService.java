e6d1501b4f5772638f187767d59e052cbf04340d
jiwon <wlldnjs@naver.com>
2017-10-05 22:03:50 +0900

    NetworkService 검색 api 작성완료

--- Seouri/app/src/main/java/sopt/seouri/network/NetworkService.java
+++ Seouri/app/src/main/java/sopt/seouri/network/NetworkService.java
@@ -1,8 +1,27 @@
 package sopt.seouri.network;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import sopt.seouri.search.category.SearchCategoryResult;
+import sopt.seouri.search.detail.SearchDetailResult;
+import sopt.seouri.search.popup.SearchPopupResult;
+
 /**
  * Created by 김지원 on 2017-10-05.
  */
 
 public interface NetworkService {
+    /*검색 API**/
+    // 카테고리별 마을기업 조회
+    @GET("villageEnterprise/category/{category}/{location}")
+    Call<SearchCategoryResult> getSearchCategoryResult(@Path("category") String category, @Path("location") String location);
+
+    // 특정 마을기업 조회
+    @GET("villageEnterprise/detail/{villageEnterpriseId}")
+    Call<SearchDetailResult> getSearchDetailResult(@Path("villageEnterpriseId") String villageEnterpriseId);
+
+    // 검색 팝업
+    @GET("villageEnterprise/{name}")
+    Call<SearchPopupResult> getSearchPopupResult(@Path("name") String name);
 }
