ce1c832fd862082997f8ed4c904540c13eb6c746
jiwon <wlldnjs@naver.com>
2017-10-31 02:40:15 +0900

    상세 검색페이지 - Tab에서 Viewpager로 전환

--- Seouri/app/src/main/java/sopt/seouri/search/category/CategoryFragment.java
+++ Seouri/app/src/main/java/sopt/seouri/search/category/CategoryFragment.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TabHost;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -20,21 +19,23 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import sopt.seouri.R;
-import sopt.seouri.adapters.RecyclerCategoryAdapter;
+import sopt.seouri.adapters.CategoryViewpagerAdapter;
 import sopt.seouri.application.ApplicationController;
 import sopt.seouri.network.NetworkService;
-import sopt.seouri.search.detail.SearchDetailFragment;
 
 import static sopt.seouri.MainActivity.fragmentManager;
 import static sopt.seouri.application.ApplicationController.serverToken;
 
 
-public class CategoryFragment extends Fragment {
+public class CategoryFragment extends Fragment implements View.OnClickListener{
     private Context context;
     private String location;
     private NetworkService service;
     private RecyclerView recyclerView1, recyclerView2, recyclerView3, recyclerView4, recyclerView5;
     private ArrayList<SearchCategoryResultData> itemData1, itemData2, itemData3, itemData4, itemData5;
+    private ViewPager viewpager;
+    private ImageView allBtn, nollBtn, muckBtn, gooBtn, chaeBtn;
+    private CategoryRecyclerView view1, view2, view3, view4, view5;
 
     public CategoryFragment() {
     }
@@ -53,9 +54,29 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         service = ApplicationController.getInstance().getNetworkService();
-        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.category_fragment, container, false);
-        TabHost tabHost = (TabHost) layout.findViewById(R.id.category_tabhost);
-        tabHost.setup();
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.category_fragment, container, false);
+//        TabHost tabHost = (TabHost) layout.findViewById(R.id.category_tabhost);
+//        tabHost.setup();
+        viewpager = (ViewPager)layout.findViewById(R.id.category_viewpager);
+        viewpager.setOffscreenPageLimit(2);
+        viewpager.setCurrentItem(0);
+
+        allBtn = (ImageView)layout.findViewById(R.id.category_all);
+        nollBtn = (ImageView)layout.findViewById(R.id.category_noll);
+        muckBtn = (ImageView)layout.findViewById(R.id.category_muck);
+        gooBtn = (ImageView)layout.findViewById(R.id.category_goo);
+        chaeBtn = (ImageView)layout.findViewById(R.id.category_chae);
+
+        allBtn.setOnClickListener(this);
+        allBtn.setTag(0);
+        nollBtn.setOnClickListener(this);
+        nollBtn.setTag(1);
+        muckBtn.setOnClickListener(this);
+        muckBtn.setTag(2);
+        gooBtn.setOnClickListener(this);
+        gooBtn.setTag(3);
+        chaeBtn.setOnClickListener(this);
+        chaeBtn.setTag(4);
 
         itemData1 = new ArrayList<>();
         itemData2 = new ArrayList<>();
@@ -63,58 +84,55 @@ public class CategoryFragment extends Fragment {
         itemData4 = new ArrayList<>();
         itemData5 = new ArrayList<>();
 
-        recyclerView1 = (RecyclerView) layout.findViewById(R.id.recycler_category1);
-        recyclerView1.setHasFixedSize(true);
-//        LinearLayoutManager manager1 = new LinearLayoutManager(context);
-//        manager1.setOrientation(LinearLayoutManager.VERTICAL);
-//        recyclerView1.setLayoutManager(manager1);
-        recyclerView1.setLayoutManager(new LinearLayoutManager(context));
+//        recyclerView1 = (RecyclerView) layout.findViewById(R.id.recycler_category1);
+//        recyclerView1.setHasFixedSize(true);
+//        recyclerView1.setLayoutManager(new LinearLayoutManager(context));
+//
+//        recyclerView2 = (RecyclerView) layout.findViewById(R.id.recycler_category2);
+//        recyclerView2.setHasFixedSize(true);
+//        recyclerView2.setLayoutManager(new LinearLayoutManager(context));
+//
+//        recyclerView3 = (RecyclerView) layout.findViewById(R.id.recycler_category3);
+//        recyclerView3.setHasFixedSize(true);
+//        recyclerView3.setLayoutManager(new LinearLayoutManager(context));
+//
+//        recyclerView4 = (RecyclerView) layout.findViewById(R.id.recycler_category4);
+//        recyclerView4.setHasFixedSize(true);
+//        recyclerView4.setLayoutManager(new LinearLayoutManager(context));
+//
+//        recyclerView5 = (RecyclerView) layout.findViewById(R.id.recycler_category5);
+//        recyclerView5.setHasFixedSize(true);
+//        recyclerView5.setLayoutManager(new LinearLayoutManager(context));
 
-        recyclerView2 = (RecyclerView) layout.findViewById(R.id.recycler_category2);
-        recyclerView2.setHasFixedSize(true);
-        recyclerView2.setLayoutManager(new LinearLayoutManager(context));
-
-        recyclerView3 = (RecyclerView) layout.findViewById(R.id.recycler_category3);
-        recyclerView3.setHasFixedSize(true);
-        recyclerView3.setLayoutManager(new LinearLayoutManager(context));
-
-        recyclerView4 = (RecyclerView) layout.findViewById(R.id.recycler_category4);
-        recyclerView4.setHasFixedSize(true);
-        recyclerView4.setLayoutManager(new LinearLayoutManager(context));
-
-        recyclerView5 = (RecyclerView) layout.findViewById(R.id.recycler_category5);
-        recyclerView5.setHasFixedSize(true);
-        recyclerView5.setLayoutManager(new LinearLayoutManager(context));
-
-        // 첫번째 탭
-        TabHost.TabSpec ts1 = tabHost.newTabSpec("Tab Spec1");
-        ts1.setContent(R.id.tab1);
-        ts1.setIndicator("전체");
-        tabHost.addTab(ts1);
-
-        // 두번째 탭
-        TabHost.TabSpec ts2 = tabHost.newTabSpec("Tab Spec2");
-        ts2.setContent(R.id.tab2);
-        ts2.setIndicator("놀거리");
-        tabHost.addTab(ts2);
-
-        // 세번째 탭
-        TabHost.TabSpec ts3 = tabHost.newTabSpec("Tab Spec3");
-        ts3.setContent(R.id.tab3);
-        ts3.setIndicator("먹거리");
-        tabHost.addTab(ts3);
-
-        // 네번째 탭
-        TabHost.TabSpec ts4 = tabHost.newTabSpec("Tab Spec4");
-        ts4.setContent(R.id.tab4);
-        ts4.setIndicator("구경거리");
-        tabHost.addTab(ts4);
-
-        // 다섯번째 탭
-        TabHost.TabSpec ts5 = tabHost.newTabSpec("Tab Spec5");
-        ts5.setContent(R.id.tab5);
-        ts5.setIndicator("체험거리");
-        tabHost.addTab(ts5);
+//        // 첫번째 탭
+//        TabHost.TabSpec ts1 = tabHost.newTabSpec("Tab Spec1");
+//        ts1.setContent(R.id.tab1);
+//        ts1.setIndicator("전체");
+//        tabHost.addTab(ts1);
+//
+//        // 두번째 탭
+//        TabHost.TabSpec ts2 = tabHost.newTabSpec("Tab Spec2");
+//        ts2.setContent(R.id.tab2);
+//        ts2.setIndicator("놀거리");
+//        tabHost.addTab(ts2);
+//
+//        // 세번째 탭
+//        TabHost.TabSpec ts3 = tabHost.newTabSpec("Tab Spec3");
+//        ts3.setContent(R.id.tab3);
+//        ts3.setIndicator("먹거리");
+//        tabHost.addTab(ts3);
+//
+//        // 네번째 탭
+//        TabHost.TabSpec ts4 = tabHost.newTabSpec("Tab Spec4");
+//        ts4.setContent(R.id.tab4);
+//        ts4.setIndicator("구경거리");
+//        tabHost.addTab(ts4);
+//
+//        // 다섯번째 탭
+//        TabHost.TabSpec ts5 = tabHost.newTabSpec("Tab Spec5");
+//        ts5.setContent(R.id.tab5);
+//        ts5.setIndicator("체험거리");
+//        tabHost.addTab(ts5);
 
         Call<SearchCategoryResult> searchCategoryResultCall = service.getSearchCategoryResult(serverToken,location);
         searchCategoryResultCall.enqueue(new Callback<SearchCategoryResult>() {
@@ -122,16 +140,6 @@ public class CategoryFragment extends Fragment {
             public void onResponse(Call<SearchCategoryResult> call, Response<SearchCategoryResult> response) {
                 if (response.isSuccessful()) {
                     if (response.body().message.equals("Succeed in selecting location") || response.body().message.equals("Succeed in selecting total location")) {
-//                        Toast.makeText(context, ""+response.body().list.get(0).name, Toast.LENGTH_SHORT).show();
-
-                        // 통신 완료 후 주석처리
-//                        itemData1.add(new SearchCategoryResultData("1", "성북구 마을기업", "사진1", "1"));
-//                        itemData1.add(new SearchCategoryResultData("2", "혜화 마을기업", "사진2", "2"));
-//                        itemData1.add(new SearchCategoryResultData("3", "성신여대 마을기업", "사진3", "3"));
-//                        itemData1.add(new SearchCategoryResultData("4", "동대문 마을기업", "사진4", "4"));
-//                        itemData1.add(new SearchCategoryResultData("5", "강남구 마을기업", "사진5", "4"));
-
-                        // 서버에 데이터 추가 된 후 주석 풀 것
                         itemData1 = response.body().list;
 
                         for(int i=0; i<itemData1.size(); i++) {
@@ -145,21 +153,21 @@ public class CategoryFragment extends Fragment {
                                 itemData5.add(itemData1.get(i));
                             }
                         }
-                    /*    RecyclerCategoryAdapter adapter1 = new RecyclerCategoryAdapter(itemData1, clickListener);
-                        RecyclerCategoryAdapter adapter1 = new RecyclerCategoryAdapter(itemData1, clickListener, context);
->>>>>>> a971fe854435870273f85f64f410196cec733da0
-                        recyclerView1.setAdapter(adapter1);
-                        RecyclerCategoryAdapter adapter2 = new RecyclerCategoryAdapter(itemData2, clickListener, context);
-                        recyclerView2.setAdapter(adapter2);
-                        RecyclerCategoryAdapter adapter3 = new RecyclerCategoryAdapter(itemData3, clickListener, context);
-                        recyclerView3.setAdapter(adapter3);
-                        RecyclerCategoryAdapter adapter4 = new RecyclerCategoryAdapter(itemData4, clickListener, context);
-                        recyclerView4.setAdapter(adapter4);
-<<<<<<< HEAD
-                        RecyclerCategoryAdapter adapter5 = new RecyclerCategoryAdapter(itemData5, clickListener);
-                        recyclerView5.setAdapter(adapter5);*/
-                        RecyclerCategoryAdapter adapter5 = new RecyclerCategoryAdapter(itemData5, clickListener, context);
-                        recyclerView5.setAdapter(adapter5);
+
+                        CategoryViewpagerAdapter viewpagerAdapter = new CategoryViewpagerAdapter(fragmentManager, context, itemData1, itemData2, itemData3, itemData4, itemData5);
+                        viewpager.setAdapter(viewpagerAdapter);
+                        viewpagerAdapter.notifyDataSetChanged();
+//                        Toast.makeText(context,"통신"+itemData1.size(), Toast.LENGTH_SHORT).show();
+//                        RecyclerCategoryAdapter adapter1 = new RecyclerCategoryAdapter(itemData1, clickListener, context);
+//                        recyclerView1.setAdapter(adapter1);
+//                        RecyclerCategoryAdapter adapter2 = new RecyclerCategoryAdapter(itemData2, clickListener, context);
+//                        recyclerView1.setAdapter(adapter2);
+//                        RecyclerCategoryAdapter adapter3 = new RecyclerCategoryAdapter(itemData3, clickListener, context);
+//                        recyclerView1.setAdapter(adapter3);
+//                        RecyclerCategoryAdapter adapter4 = new RecyclerCategoryAdapter(itemData4, clickListener, context);
+//                        recyclerView1.setAdapter(adapter4);
+//                        RecyclerCategoryAdapter adapter5 = new RecyclerCategoryAdapter(itemData5, clickListener, context);
+//                        recyclerView5.setAdapter(adapter5);
                     }
                 } else {
                     Log.d("SearchCategory 통신 에러", "");
@@ -171,56 +179,79 @@ public class CategoryFragment extends Fragment {
                 Log.d("SearchCategory", "통신 실패");
             }
         });
-
-//        itemData1.add(new SearchCategoryResultData("1","성북구 마을기업","사진1","1"));
-//        itemData1.add(new SearchCategoryResultData("2","혜화 마을기업","사진2","1"));
-//        itemData1.add(new SearchCategoryResultData("3","성신여대 마을기업","사진3","1"));
-
         return layout;
     }
 
-    public View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            int position;
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            SearchDetailFragment searchDetailFragment = new SearchDetailFragment();
+//    public View.OnClickListener clickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            int position;
+//            FragmentTransaction transaction = fragmentManager.beginTransaction();
+//            SearchDetailFragment searchDetailFragment = new SearchDetailFragment();
+//
+//            switch (((View)view.getParent()).getId()){
+//                case R.id.recycler_category1 :
+//                    position = recyclerView1.getChildPosition(view);
+////                    Toast.makeText(context,"첫번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData1.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+//                    searchDetailFragment.setContext(context, String.valueOf(itemData1.get(position).villageEnterpriseId));
+//                    break;
+//
+//                case R.id.recycler_category2 :
+//                    position = recyclerView2.getChildPosition(view);
+////                    Toast.makeText(context,"두번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData2.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+//                    searchDetailFragment.setContext(context, String.valueOf(itemData2.get(position).villageEnterpriseId));
+//                    break;
+//
+//                case R.id.recycler_category3 :
+//                    position = recyclerView3.getChildPosition(view);
+////                    Toast.makeText(context,"세번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData3.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+//                    searchDetailFragment.setContext(context, String.valueOf(itemData3.get(position).villageEnterpriseId));
+//                    break;
+//
+//                case R.id.recycler_category4 :
+//                    position = recyclerView4.getChildPosition(view);
+////                    Toast.makeText(context,"네번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData4.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+//                    searchDetailFragment.setContext(context, String.valueOf(itemData4.get(position).villageEnterpriseId));
+//                    break;
+//
+//                case R.id.recycler_category5 :
+//                    position = recyclerView5.getChildPosition(view);
+////                    Toast.makeText(context,"다섯번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData5.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+//                    searchDetailFragment.setContext(context, String.valueOf(itemData5.get(position).villageEnterpriseId));
+//                    break;
+//            }
+//
+//            transaction.replace(R.id.container,searchDetailFragment);
+//            transaction.addToBackStack(null);
+//            transaction.commit();
+//        }
+//    };
 
-            switch (((View)view.getParent()).getId()){
-                case R.id.recycler_category1 :
-                    position = recyclerView1.getChildPosition(view);
-//                    Toast.makeText(context,"첫번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData1.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
-                    searchDetailFragment.setContext(context, String.valueOf(itemData1.get(position).villageEnterpriseId));
-                    break;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.category_all:
 
-                case R.id.recycler_category2 :
-                    position = recyclerView2.getChildPosition(view);
-//                    Toast.makeText(context,"두번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData2.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
-                    searchDetailFragment.setContext(context, String.valueOf(itemData2.get(position).villageEnterpriseId));
-                    break;
+                break;
+            case R.id.category_noll:
 
-                case R.id.recycler_category3 :
-                    position = recyclerView3.getChildPosition(view);
-//                    Toast.makeText(context,"세번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData3.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
-                    searchDetailFragment.setContext(context, String.valueOf(itemData3.get(position).villageEnterpriseId));
-                    break;
+                break;
+            case R.id.category_muck:
 
-                case R.id.recycler_category4 :
-                    position = recyclerView4.getChildPosition(view);
-//                    Toast.makeText(context,"네번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData4.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
-                    searchDetailFragment.setContext(context, String.valueOf(itemData4.get(position).villageEnterpriseId));
-                    break;
+                break;
+            case R.id.category_goo:
 
-                case R.id.recycler_category5 :
-                    position = recyclerView5.getChildPosition(view);
-//                    Toast.makeText(context,"다섯번째 recycler의 " +(position+1)+"번째 item클릭,"+itemData5.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
-                    searchDetailFragment.setContext(context, String.valueOf(itemData5.get(position).villageEnterpriseId));
-                    break;
-            }
+                break;
+            case R.id.category_chae:
 
-            transaction.replace(R.id.container,searchDetailFragment);
-            transaction.addToBackStack(null);
-            transaction.commit();
+                break;
         }
-    };
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
 }
---
+++ Seouri/app/src/main/java/sopt/seouri/search/category/CategoryRecyclerView.java
@@ -0,0 +1,69 @@
+package sopt.seouri.search.category;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import sopt.seouri.R;
+import sopt.seouri.adapters.RecyclerCategoryAdapter;
+import sopt.seouri.network.NetworkService;
+import sopt.seouri.search.detail.SearchDetailFragment;
+
+import static sopt.seouri.MainActivity.fragmentManager;
+
+/**
+ * Created by 김지원 on 2017-10-30.
+ */
+
+public class CategoryRecyclerView extends Fragment{
+    Context context;
+    ArrayList<SearchCategoryResultData> itemData;
+    RecyclerView recyclerView;
+    NetworkService service;
+
+    public CategoryRecyclerView() {
+    }
+
+    public CategoryRecyclerView(Context context, ArrayList<SearchCategoryResultData> itemData){
+        this.context = context;
+        this.itemData = itemData;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.category_viewpager,container,false);
+        recyclerView = (RecyclerView)layout.findViewById(R.id.category_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        RecyclerCategoryAdapter adapter = new RecyclerCategoryAdapter(itemData, clickListener, context);
+        recyclerView.setAdapter(adapter);
+
+        return layout;
+    }
+
+    public View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int position;
+            position = recyclerView.getChildPosition(v);
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            SearchDetailFragment searchDetailFragment = new SearchDetailFragment(context, itemData.get(position).villageEnterpriseId);
+
+            transaction.replace(R.id.container,searchDetailFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+//            Toast.makeText(context,""+(position+1)+"번째 item클릭,"+itemData.get(position).villageEnterpriseId,Toast.LENGTH_SHORT).show();
+        }
+    };
+}
